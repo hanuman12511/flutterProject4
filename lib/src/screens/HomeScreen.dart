388a8d3cedd3fc752077c0e1eb5e5653a9492423
hanuman12511 <er.hanumansaini@gmail.com>
2023-06-08 23:23:08 +0530

    fff

--- lib/src/screens/HomeScreen.dart
+++ lib/src/screens/HomeScreen.dart
@@ -38,6 +38,7 @@ class HomeScreen extends StatelessWidget {
     );
   }
 
+  dateController() {}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -80,6 +81,27 @@ class HomeScreen extends StatelessWidget {
               height: 100,
               margin: EdgeInsets.only(top: 30),
               color: Color.fromARGB(255, 233, 6, 6),
+              child: TextField(
+                  //editing controller of this TextField
+                  decoration: const InputDecoration(
+                      icon: Icon(Icons.calendar_today), //icon of text field
+                      labelText: "Enter Date" //label text of field
+                      ),
+                  readOnly: true, // when true user cannot edit text
+                  onTap: () async {
+                    DateTime? pickedDate = await showDatePicker(
+                        context: context,
+                        initialDate: DateTime.now(), //get today's date
+                        firstDate: DateTime(
+                            2000), //DateTime.now() - not to allow to choose before today.
+                        lastDate: DateTime(2101));
+                    if (pickedDate != null) {
+                      print(
+                          pickedDate); //get the picked date in the format => 2022-07-04 00:00:00.000
+                    } else {
+                      print("Date is not selected");
+                    }
+                  }),
             ),
           ),
           Container(
@@ -88,16 +110,85 @@ class HomeScreen extends StatelessWidget {
               child: Column(children: [
                 Container(
                   height: 300,
-                  color: Colors.amber,
+                  color: Color.fromARGB(255, 40, 255, 7),
+                  child: ListTile(
+                      hoverColor: Colors.amber,
+                      trailing: Icon(Icons.access_alarm_sharp),
+                      title: Text("data"),
+                      subtitle: Text("data"),
+                      leading: CircleAvatar(
+                        child: Image(
+                          image: AssetImage('assets/images/logo.png'),
+                        ),
+                      )),
                 ),
                 Container(
                   height: 300,
-                  color: Color.fromARGB(255, 156, 7, 255),
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/img.jpg'),
+                        fit: BoxFit.cover,
+                        colorFilter:
+                            ColorFilter.mode(Colors.black54, BlendMode.darken)),
+                  ),
                 ),
                 Container(
-                  child: Image.asset('assets/images/$img'),
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                  child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          padding: EdgeInsets.only(top: 10),
+                          width: 200,
+                          child: Row(children: [
+                            Container(
+                              margin: EdgeInsets.all(0),
+                              child: Icon(
+                                Icons.arrow_back_ios_sharp,
+                                color: Colors.amber,
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.all(0),
+                              child: Text(
+                                "data",
+                                style: TextStyle(
+                                    color: Colors.amber, fontSize: 25),
+                              ),
+                            )
+                          ]),
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                              color: Colors.amber,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(20)),
+                              border: Border.all(
+                                color: Color.fromARGB(255, 255, 255, 255),
+                              )),
+                          child: Text(
+                            "Booking",
+                            style: TextStyle(
+                                color: Color.fromARGB(255, 255, 255, 255),
+                                fontSize: 20),
+                          ),
+                        ),
+                      ]),
+                  margin: EdgeInsets.all(10),
                   height: 300,
-                  color: Color.fromARGB(255, 255, 226, 7),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                        style: BorderStyle.solid,
+                        color: Color.fromARGB(255, 238, 0, 0)),
+                    borderRadius: BorderRadius.circular(30),
+                    image: DecorationImage(
+                        image: AssetImage("assets/images/img1.jpg"),
+                        fit: BoxFit.fitHeight,
+                        colorFilter: ColorFilter.mode(
+                            Colors.black87, BlendMode.difference)),
+                  ),
                 )
               ]),
             ),
